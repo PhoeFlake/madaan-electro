251e8eec1d0594caf5879ba0c872c7e966fb1fee
PhoeFlake <drishtimadaan2004@gmail.com>
2025-12-16 10:05:29 +0530

    Initial commit
    - Add project setup with Vite
    - Configure ESLint
    - Add README and gitignore
    - Create App component with styling
    - Set up main entry point

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="top-strip py-2 border-b- border-t-1">
+        <div className="container">
+            <div className="flex items-center justify-between">
+                <div className="col1 w-[50%]">
+                    <p className="text-[14px]">Free Shipping Over $100 & Free Returns</p> 
+                </div>
+            </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
